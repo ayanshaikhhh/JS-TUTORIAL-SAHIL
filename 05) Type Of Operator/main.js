53a7a24371cf82c6ab5fa9f8fe38253c143fa343
mohammed ayan <mohammedayan71636@gmail.com>
2025-09-12 10:37:27 +0530

    update

---
+++ 05) Type Of Operator/main.js
@@ -0,0 +1,146 @@
+// Arithmetic Operators:
+// let a = 5;
+// let b = 5;
+// console.log(a+b);
+
+
+
+
+
+
+
+// ----------------------------------------Assignment Operators..-----------
+// Assignment Operators..
+// let c = 11;
+// console.log(c);
+
+
+
+// let x = 5;
+// x+=3;  // x = 5 + 3
+// console.log(x);
+
+
+
+// let x = 5;
+// x+=3;
+// console.log(x); // 8
+
+
+// let x = 12;
+// let y = 6;
+
+// y+=x;  // y = y+x;
+// console.log(y); // 18
+
+
+
+
+// ---------------------------------------------------
+// let a = 4;  // 4
+// a*=3 + 2;  // a = a * (3+2) = 4*5 = 20
+// console.log(a); 
+
+
+
+
+// x+=1; 
+// console.log(x);
+
+
+
+// let x = 10; // 10
+// x -= x *=2; // x = 10 - (10*2)   
+// console.log(x);
+
+
+
+
+// let x = 2;  // 2
+// x**=3; // x = x**3 = 2**3 = 2*2*2 = 8
+// console.log(x);
+
+
+
+
+
+
+// let a = 5;
+// let b = 10;
+// a += b -= a;    // a = a + b - a         5 + 10 - 5
+
+
+
+
+
+
+// ---------------------------------------------------Comparison Operators..---------
+// console.log(50 > 5); 
+
+
+
+
+
+
+
+// -------------------------------------------Logical Operators..---------------------------------
+// Logical Operators..
+// AND Operator (&&)
+// OR Operator (||)
+// NOT Operator (!)
+
+// if (10>5 && 5>1) {
+//     console.log("and operator is true");
+// } else {
+//     console.log("and operator is false");
+// };
+
+
+
+// if (10>5 && 5>1) {
+//     console.log("or operator is true");
+// } else {
+//     console.log("or operator is false");
+// }
+
+
+// if(5!=5){
+//     console.log("true")
+// } else {
+//     console.log("false");
+// }
+
+
+
+
+
+console.log(5<2 && 5>3);
+console.log(5<2 || 5>3);
+console.log(5 != 5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
