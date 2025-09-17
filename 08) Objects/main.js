f829240cab1ee5af2927549f3b59682b0b828151
mohammed ayan <mohammedayan71636@gmail.com>
2025-09-17 12:53:57 +0530

    update

---
+++ 08) Objects/main.js
@@ -0,0 +1,157 @@
+// -----------------------------Objects------------------------------
+// let user = Symbol("sajid")
+// let obj = {
+//     name : "sahbaj",
+//     age : 23,
+//     city : "ajmer",
+//     [user] : "sajid"
+// };
+
+// console.log(obj);
+// console.log(obj.name);
+// console.log(obj["name"]);
+// console.log(obj["age"]);
+// obj.state = "rajasthan";
+
+
+
+
+// console.log(obj)
+// console.log(obj[user]);
+
+
+
+
+// obj.name = "atif";  // override name
+// console.log(obj);
+
+
+
+
+// Object.freeze(obj);  // value freez
+// obj.name = "sahadat";  // override name
+// console.log(obj);
+// obj.age = 55;  // value freez
+// console.log(obj);
+
+
+
+
+
+
+// let s = obj.greeting = function () {
+//     console.log("function");
+// };
+
+
+// obj.greeting = function () {
+//     console.log(`Hello JS users ${this.name}`);
+// };
+
+// // console.log(s);
+// // console.log(obj);
+
+// console.log(obj.greeting());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -----------------------------------------Literal syntax (curly braces)-------------------------------
+// const obj = { name: "Rahul", age: 25 };
+// console.log(obj);
+
+
+
+
+
+// -------------------------------------Constructor new Object()----------------------------------
+// const obj2 = new Object();
+// obj2.name = "sameer";
+// obj2.age = "21";
+// console.log(obj2);
+
+
+
+
+
+
+// -------------------------------------Maths-----------------------------------------
+// console.log(Math.PI);  // 3.14
+
+// console.log(Math.floor(4.9));  // 4
+
+// console.log(Math.ceil(4.1));  // 5
+
+// console.log(Math.round(4.3));  // [4] round value min & max
+
+// console.log(Math.round(4.8));  // [5] round value min & max
+
+// console.log(Math.floor(Math.random()) ) // Random number
+
+
+
+
+
+// // ----------------------Random----------------------
+// let pizza = ["cheez pizza", "paneer pizza", "peppy paneer pizza"];
+// let num = Math.floor(Math.random() * 3);
+// console.log(pizza[num]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
