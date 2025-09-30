3ae91b872b9f9d306152fb628ce615815db487ab
mohammed ayan <mohammedayan71636@gmail.com>
2025-09-30 18:06:02 +0530

    update

---
+++ 16) BOM/main.js
@@ -0,0 +1,54 @@
+// BOM in JavaScript ka matlab hai Browser Object Model.
+// Ye browser ke components (window, screen, location, history, navigator, etc.) ko JavaScript ke through control karne ka ek model hai.
+
+// 🔹 BOM (Browser Object Model) kya hai?
+// JavaScript ke through aap browser ke alag-alag features ko access aur manipulate kar sakte ho.
+// Example: Window open/close karna, URL change karna, history check karna, screen ke size ka data lena, etc.
+// BOM ka root object window hota hai.
+
+
+
+
+
+
+// 🔹 Summary
+// DOM (Document Object Model) → HTML ke elements ko represent karta hai.
+// BOM (Browser Object Model) → Browser ko represent karta hai.
+// 👉 Simple: DOM = webpage ka content, BOM = browser ke features.
+
+
+
+
+
+
+console.log(window.innerHeight)
+console.log(window.outerHeight)
+console.log(screen.height)
+console.log(screen.width)
+console.log(location)
+console.log(location.href)
+// location.href = "https://google.com";
+history.back;
+history.forward;
+console.log(navigator.appName)
+console.log(navigator.language)
+console.log(navigator.onLine)
+alert("Hello");
+let yes = confirm("are you sure")
+let name = prompt("Enter your name");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
