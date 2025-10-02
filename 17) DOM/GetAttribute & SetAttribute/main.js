474affe7d1d0664aef9ac9765e8195e0499e18d0
mohammed ayan <mohammedayan71636@gmail.com>
2025-10-02 17:21:37 +0530

    update

---
+++ 17) DOM/GetAttribute & SetAttribute/main.js
@@ -0,0 +1,18 @@
+
+// let el = document.querySelector('[ayan="shaikh"]')
+// console.log(el.innerHTML);
+
+
+
+// const pr = document.querySelector('[procoder]');
+// console.log(pr)
+
+
+
+// const pr = document.querySelector('[procoder="akash"]');
+// console.log(pr.innerHTML);
+
+
+
+let copy = document.querySelector("body > p");
+console.log(copy)
