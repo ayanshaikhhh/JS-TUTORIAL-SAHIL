b27b3d8d910c32beac2dfd0bc47ef97ef6c8c1f6
mohammed ayan <mohammedayan71636@gmail.com>
2025-10-09 19:48:14 +0530

    update

---
+++ 17) DOM/Remove Element/main.js
@@ -0,0 +1,52 @@
+//     let btn = document.querySelector("button");
+//     btn.style.backgroundColor = "orange";
+// function removeElement() {
+//     let p = document.querySelector("#demo");
+
+//     p.remove();
+// }
+
+
+
+
+
+
+
+
+
+
+
+// function clearContainer() {
+//     let cont = document.querySelector("#container");
+//     cont.innerHTML = "";
+//     // cont.remove();
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// let list = document.querySelector("#list");
+// // let item = list.querySelector("li:nth-child(2)");
+// list.remove();
+
+
+
+
+
+
+
+
+
+
+
+
+let boxes = document.querySelectorAll(".box");
+boxes.remove();
